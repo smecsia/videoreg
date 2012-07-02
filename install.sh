97a36f42d1d55242d493d80101a7bdb296b7b1f3
Ilya Sadykov <smecsia@gmail.com>
2012-07-02 19:03:20 +0400

    install script

--- install.sh
+++ install.sh
@@ -13,7 +13,7 @@ fi
 apt-get install mencoder ffmpeg qc-usb-utils v4l-utils rabbitmq-server ruby1.9.1 rubygems
 cd $CURDIR
 gem install bundler --no-rdoc --no-ri
-if [ -f /var/lib/gems/1.8/bin/bundle ]
+if [ -f /var/lib/gems/1.8/bin/bundle ];
 then
     export BUNDLER=/var/lib/gems/1.8/bin/bundle
 else
@@ -26,5 +26,5 @@ then
     gem build videoreg.gemspec
     gem install videoreg-0.1.gem
 else
-    echo "Bundler executable not found! Installation FAILED!"
+    echo "Bundler executable not found ($BUNDLER)! Installation FAILED!"
 fi
