dc3c4cca71f7b57e66d7f17149c09800a130ee77
Ilya Sadykov <smecsia@gmail.com>
2012-07-02 19:07:31 +0400

    install script

--- install.sh
+++ install.sh
@@ -11,6 +11,9 @@ else
     apt-get update
 fi
 apt-get install mencoder ffmpeg qc-usb-utils v4l-utils rabbitmq-server ruby1.9.1 rubygems
+ln -sf /usr/bin/ruby1.9.1 /usr/bin/ruby
+ln -sf /usr/bin/rake1.9.1 /usr/bin/rake
+ln -sf /usr/bin/gem1.9.1 /usr/bin/gem
 cd $CURDIR
 gem install bundler --no-rdoc --no-ri
 if [ -f /var/lib/gems/1.8/bin/bundle ];
