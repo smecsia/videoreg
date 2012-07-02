5e7e6336c9994b6c6813634535c4c9d6ef6ceeb5
Ilya Sadykov <smecsia@gmail.com>
2012-07-02 18:54:06 +0400

    install script

--- install.sh
+++ install.sh
@@ -1,12 +1,18 @@
 #!/bin/bash
 CURDIR=`pwd`
 cd /tmp
-echo "deb http://www.rabbitmq.com/debian/ testing main" >> /etc/apt/sources.list
-wget http://www.rabbitmq.com/rabbitmq-signing-key-public.asc
-apt-key add rabbitmq-signing-key-public.asc
-apt-get update
+if grep -q "deb http://www.rabbitmq.com/debian/ testing main" /etc/apt/sources.list
+then
+    echo "It seems you are installing env second time..."
+else
+    echo "deb http://www.rabbitmq.com/debian/ testing main" >> /etc/apt/sources.list
+    wget http://www.rabbitmq.com/rabbitmq-signing-key-public.asc
+    apt-key add rabbitmq-signing-key-public.asc
+    apt-get update
+fi
 apt-get install mencoder ffmpeg qc-usb-utils v4l-utils rabbitmq-server ruby1.9.1 rubygems
 cd $CURDIR
+gem install bundler
 bundle install
 gem build videoreg.gemspec
 gem install videoreg-0.1.gem
